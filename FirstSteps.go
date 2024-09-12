e1b9d729b97d56da748ec620f1c6e626fda37a88
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-09-12 20:24:04 +0300

    Âîò òàê

---
+++ FirstSteps.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	numbers := []int{1, 2, 3, 4, 5}
+// 	fmt.Println("Nums", numbers)
+
+// 	//and what
+// }
+// func main() {
+// 	// initialize variables here
+// 	var smsSendingLimit int
+// 	var costPerSMS float64
+// 	var hasPermission bool
+// 	var username string
+// 	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+// }
