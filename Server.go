f4b12ea4a7e5ef23c3d5c39ecfd2c7658d8918e4
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-09-13 23:22:07 +0300

    Struct

--- Server.go
+++ Server.go
@@ -5,15 +5,39 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
 //	func home_page(page http.ResponseWriter, r *http.Request) {
 //		fmt.Fprintf(page, "Thunder")
 //	}
-func home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "Thunder")
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
 }
 
-func main() {
+func home_page(page http.ResponseWriter, r *http.Request) {
+	bob := User{"Babuleh", 18, -1000, 5.0, 1.0}
+	// fmt.Fprintf(page, "User name is "+bob.name)
+	fmt.Fprint(page, bob.getAllInfo())
+}
+
+func babuleh_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Babuleh is here")
+}
+
+func HandleRequest() {
 	http.HandleFunc("/", home_page)
+	http.HandleFunc("/babuleh/", babuleh_page)
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("obed")
+}
+func main() {
+	//var bob User = ....
+	//bob := User{name: "babuleh", age:18, money:-1000, avg_grades:5, happiness: 1}
+
+	HandleRequest()
 }
