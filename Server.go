37da3fbbda563c0fd55897765b67b1d77b1bad1d
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-09-13 22:36:48 +0300

    Yes

--- Server.go
+++ Server.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+//	func home_page(page http.ResponseWriter, r *http.Request) {
+//		fmt.Fprintf(page, "Thunder")
+//	}
+func home_page(page http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(page, "Thunder")
+}
 
 func main() {
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
 	fmt.Println("obed")
 }
