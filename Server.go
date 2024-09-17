3d87ce010f375bf4e30137abf7840892daa9e7c9
Babuleh78 <jdbdbsbbfdh@gmail.com>
2024-09-17 16:47:28 +0300

    Template

--- Server.go
+++ Server.go
@@ -7,10 +7,11 @@ import (
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
 }
 
 //	func home_page(page http.ResponseWriter, r *http.Request) {
@@ -18,11 +19,11 @@ type User struct {
 //	}
 
 func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
 }
 
 func home_page(page http.ResponseWriter, r *http.Request) {
-	bob := User{"Babuleh", 18, -1000, 5.0, 1.0}
+	bob := User{"Babuleh", 18, -1000, 5.0, 1.0, []string{"a", "b", "c"}}
 	// fmt.Fprintf(page, "User name is "+bob.name)
 	//fmt.Fprint(page, `<b> Main Text </b> <h1> Main Text</h1>`)
 	tmpl, _ := template.ParseFiles("templates/home_page.html")
